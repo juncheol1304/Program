871d659bb935ee934006d6201ee98ea144c55b42
juncheol1304 <juncheol1304@naver.com>
2024-10-30 21:45:34 +0900

    (2024.10.30) 템플릿 특수화

--- Program.cpp
+++ Program.cpp
@@ -1,86 +1,46 @@
 ﻿#include <iostream>
-#include "Computer.h"
-
 
 using namespace std;
 
-class Vector2
+template<typename T>
+class List
 {
 private:
-	int x;
-	int y;
-public:
-	Vector2(int x, int y)
-	{
-#pragma region this 포인터
-		// 자기 자신을 가리키는 포인터입니다.
-		this->x = x;
-		this->y = y;
-
-#pragma endregion
-	}
-	int & X() { return x; }
-	int & Y() { return y; }
-
-	Vector2 & operator +(const Vector2 & clone)
-	{
-		Vector2 vector(this->x + clone.x, this->y + clone.y);
-
-		return vector;
-	}
-	Vector2 & operator ++()
-	{
-		this->x++;
-		this->y++;
-
-		return *this;
-	}
-	Vector2& operator ++(int value)
-	{
-		Vector2 clone(this->x, this->y);
-
-		this->x += 1;
-		this->y += 1;
-
-		return clone;
-	}
+	int size;
 	
-};
+	T* pointer;
 
-// 벡터 2클래스 만들기 x,y 값이 있어야함
-// private : int x; , int y;
-// public: int X(){반환}; int Y(){반환};
+public:
+	List(int size)
+	{
+		this->size = size;
+
+		pointer = new T[size];
+	}
+
+	void add(T data)
+	{
+
+
+	}
+
+	~List()
+	{
+		delete pointer;
+	}
+};
 
 int main()
 {
-#pragma region 다중 상속
-	// 하나의 하위 클래스가 여러 개의 상위 클래스를
-	// 상속받는 상속입니다.
+#pragma region 템플릿
+	// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터
+	// 타입들을 가질 수 있는 기술에 중점을 두어 재사용을 높일 수
+	// 있는 기능입니다.
 
-	// Computer computer;
-	 
-	// computer.Use();
-
-	// 다중 상속의 경우 여러 개의 상위 클래스에 중복되는 속성
-	// 속성이 존재할 수 있기 때문에 범위
-	// 지정 연산자를 통해 상위 클래스의 이름을
-	// 선언하고 속성을 사용 해야 합니다.
-
+	List<int> list(5);
 
 #pragma endregion
 
-#pragma region 연산자 오버로딩
-	Vector2 vector1(1, 1);
-	Vector2 vector2(3, 3);
-
-	Vector2 position = vector1 + vector2;
-	 
-	cout << ++position.X() << endl;
-
-	cout << "position x의 값 : " << position.X() << endl;
-	cout << "position y의 값 : " << position.Y() << endl;
-
-#pragma endregion
 
 
 	return 0;
