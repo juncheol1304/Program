03a5f72d97f4013d748b866e6550be867a601977
juncheol1304 <juncheol1304@naver.com>
2024-11-06 21:45:44 +0900

    (2024.11.6) 컨테이너

--- Program.cpp
+++ Program.cpp
@@ -1,40 +1,67 @@
 ﻿#include <iostream>
-#include <vector>
-#include <list>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
 int main()
 {
-#pragma region Vector 컨테이너
+#pragma region 컨테이너 어댑터
+	// 기존의 컨테이너의 인터페이스를 제한하여 만든 기능이
+	// 제한되거나 변형되어 있는 컨테이너입니다.
 
-	// std::vector<int> vector;
-	// 
-	// vector.reserve(6);
-	// 
-	// vector.push_back(10); // [10]
-	// vector.push_back(20); // [10][20]
-	// vector.push_back(30); // [10][20][30]
-	// vector.push_back(40); // [10][20][30][40]
-	// vector.push_back(50); // [10][20][30][40][50]
-	// 
-	// vector.pop_back(); // [10][20][30][40]
-	// 
-	// cout << "Capacity : " << vector.capacity() << endl;
-	// cout << "size : " << vector.size() << endl;
-	// 
-	// for (int i = 0; i < vector.size(); i++)
+#pragma region Stack
+
+	// std::stack<int> stack;
+	 
+	// stack.push(10); // [10]
+	// stack.push(20); // [10][20]
+	// stack.push(30); // [10][20][30]
+	// stack.push(40); // [10][20][30][40]
+	 
+	// stack.pop();	// [30][20][10]
+	 
+	// cout << "Stack의 top의 값 : " << stack.top() << endl;
+	// cout << "Stack의 size의 값 : " << stack.size() << endl;
+	// cout << "Stack의 empty의 값 : " << stack.empty() << endl;
+	
+	// while (stack.empty() == false)
 	// {
-	// 	cout << vector[i] << endl;
+	// 	cout << stack.top() << endl;
+	 
+	// 	stack.pop();
 	// }
-
-#pragma endregion
-
-#pragma region List 컨테이너
 	
 
+#pragma endregion
+
+#pragma region Queue
+
+	std::queue<int> queue;
+
+	queue.push(10); // [10]
+	queue.push(20); // [10][20]
+	queue.push(30); // [10][20][30]
+
+	queue.pop();	// [10][20]
+
+	cout << "Queue의 front의 값 : " << queue.front() << endl;
+	cout << "Queue의 size의 값 : " << queue.size() << endl;
+	cout << "Queue값 empty의 값: " << queue.empty() << endl;
+
+	while (queue.empty() == false)
+	{
+		cout << queue.front() << endl;
+		
+		queue.pop();
+	}
 
 #pragma endregion
+
+
+#pragma endregion
+
+
 
 	return 0;
 }
